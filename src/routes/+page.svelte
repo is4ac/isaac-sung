af59ce545756cc43ace208eb3f89adb14359336c
is4ac <isaacsung@gmail.com>
2024-09-23 08:52:11 -0500

    add tailwind and some other initial config settings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Isaac-Sung.com</h1>
+<p>I'm still working on my site. Please visit again later!</p>
