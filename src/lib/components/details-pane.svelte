4e4e5012db174160a2d168facb3c70f36cfe2f85
Isaac Sung <isaacsung@gmail.com>
2026-05-05 21:06:35 -0500

    change colors to red

--- src/lib/components/details-pane.svelte
+++ src/lib/components/details-pane.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <div
-	class="flex min-h-[20dvh] w-full flex-col gap-4 rounded-lg border-2 border-border bg-darkGray px-6 py-4"
+	class="flex min-h-[20dvh] w-full flex-col gap-4 rounded-lg border-2 border-red bg-bgred px-6 py-4"
 >
 	<h3>{title}</h3>
 	<p class="leading-relaxed">{description}</p>
